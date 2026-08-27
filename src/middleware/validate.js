8af6325137a7eeaec17d397a83d730e31a2311da
leminhkhai345 <khaile.03042005@gmail.com>
2026-08-27 15:49:07 +0700

    feat: add validate for create and update user

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -38,7 +38,26 @@ export const createUserRules = [
         .isEmail().withMessage('Email is not valid')
         .normalizeEmail(),
 
-    body('age')
-        .optional()
-        .isInt({ min: 1, max: 120 }).withMessage('Age must be an integer between 1 and 120'),
+    body('password')
+        .trim()
+        .notEmpty('password is required')
+        .isLength({ min: 6, max: 15 }).withMessage('password must be at least 6 charactor'),
+];
+
+export const updateUserRules = [
+    body('name')
+        .trim()
+        .notEmpty().withMessage('Name is required')
+        .isLength({ min: 2, max: 50 }).withMessage('Name must be between 2 and 50 characters'),
+
+    body('email')
+        .trim()
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email is not valid')
+        .normalizeEmail(),
+
+    body('password')
+        .trim()
+        .notEmpty('password is required')
+        .isLength({ min: 6, max: 15 }).withMessage('password must be at least 6 charactor'),
 ];
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
 import * as userController from "../controller/users.controller.js";
+import { createUserRules, updateUserRules, validate } from "../middleware/validate.js";
 
 const router = new Router();
 
 router.get("/", userController.getAllUsers);
 router.get("/:id/", userController.getUserById);
-router.post("/", userController.createUser);
-router.put("/:id/", userController.updateUser);
+router.post("/", validate(createUserRules), userController.createUser);
+router.put("/:id/", validate(updateUserRules), userController.updateUser);
 router.delete("/:id/", userController.deleteUser);
 export default router;
